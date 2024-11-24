e41d95530c78fe8bfbf760238129498e125728a0
Zachary Lamb <102142742+zlamb1@users.noreply.github.com>
2024-11-24 07:02:31 -0600

    added TailwindCSS and switched build system to Vite

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  corePlugins: {
+    preflight: false,
+  },
+  important: '#root',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
